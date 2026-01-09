f990a80d963c9b8a4a1e084bb1fa37319125bb3e
ChiKien_Luu <luukien2910@gmail.com>
2026-01-10 00:17:15 +0700

    update part-02-sql

---
+++ part-02-sql/section-01-basic-select/08-select-like.sql
@@ -0,0 +1,46 @@
+﻿use Northwind
+
+----------------------------------------------
+-- Lí thuyết
+-- Cú pháp mở rộng của select 
+-- Trong thực tế có những lúc ta muốn tìm dữ liệu/filter theo kiểu gần đúng
+-- gần đúng trên kiểu chuỗi, liệt kê ai đó có tên là AN, khác câu
+-- liệt ke ai đố tên bắt dầu bằng chữ A
+-- tìm đúng, toán tử = 'AN'
+-- tìm gần đúng, tìm có sự xuất hiện, không dùng =, dùng toán tử like
+--						LIKE 'A...' ...
+-- Để sử dụng toán tử like, ta cần thêm 2 thứ trọ giúp, dấu % và dấu _
+-- % đại diện cho 1 chuỗi bất kì nào đó
+-- _ đại diện cho 1 kí tự bất kì nào đó
+-- VÍ DỤ: Name like 'A%', bất kì ai có tên xh bằng chứ A, phần còn lại là gì thì kh care
+--		  Name 'A_', bất kì ai có tên là 2 kí tự, trong đó kí tự đầu tiên phải là A
+
+----------------------------------------------
+-- 1. In ra danh sách nhân viên
+select * from Employees
+
+--2. nhân viên nào có tên bắt dầu chữ A
+select * from Employees where FirstName = 'A%'--0 vì toán tử ss bằng A%, có ai tên bằng A%
+select * from Employees where FirstName like 'A%'
+
+--2.1  Nhân viên nào có tên bắt dầu chữ A, in ra củ fullname được ghép đầy đủ
+select EmployeeID, CONCAT(FirstName, ' ', LastName) as FullName, Title from Employees 
+						where FirstName like 'A%'
+
+-- 3. Nhân viên nào có tên có chữ A cuối cùng
+select EmployeeID, CONCAT(FirstName, ' ', LastName) as FullName, Title from Employees 
+						where FirstName like '%A'
+
+--4. nhân viên nòa có tên có 4 kí tự
+select * from Employees where FirstName like '____' --1
+
+--5.Xem danh sách các sản phẩm/món đồ đang có - Product
+select * from Products where ProductName like '%ch'
+select * from Products where ProductName like '%ch%'
+
+--6. Những sản phẩm trong tên sản phâm rmaf từ cuối cùng là 5 kí tự
+select * from Products where ProductName like '%_____'-- tên có từ 5 kí tự trở lên
+select * from Products where ProductName like '% _____' -- tên có ít nhất 2 từ
+														-- và từ cuối cùng có 5 kí tự  nhưng nó lại vô tình loại đi tên có đúng 5 kí tự
+
+select * from Products where ProductName like '% _____' or ProductName like '_____'
---
+++ part-02-sql/section-02-sub-query/01-single-value.sql
@@ -0,0 +1,71 @@
+﻿use Northwind
+
+----------------------------------------------
+-- Lí thuyết
+-- Cú pháp chuẩn của lệnh select
+-- select * from <table> where ....
+-- where cột = value nào đó
+-- where cột like pattern nào đó e.g. '_____'
+-- where cột between range
+-- where cột in ( tập hợp giá trị được liệt kê)
+
+-- một câu select tùy cách viết thì có thể trả về đúng 1 value/cell
+-- một câu select tùy cách viết thì có thể trả về đúng 1 tập giá trị/ 1 tập cell
+-- Tập kết quả này đồng nhất (các giá trị khác nhau của 1 biến)
+
+--*****
+-- where cột = value nào đó - đã học, e.g. year(dob) = 2003
+--			= thay value này = 1 câu sql khác miễn tả về 1 cell tương ứng
+-- kĩ thuật viết câu sql theo kiểu hỏi gián tiếp, lồng nhau
+
+
+----------------------------------------------
+-- Thực hành
+--1. IN ra danh sách nhân viên
+select * from Employees
+select FirstName from Employees where EmployeeID = 1 --1 cell
+select FirstName from Employees --1 tập giá trj/ 1 cột/ phép chiếu
+
+---2. liệt kê các nhân viên ở london
+select * from Employees where City = 'London' --4
+
+--3. liệt kê các nhân viên cùng quê với King Robert
+select * from Employees where FirstName = 'Robert'
+
+select City from Employees where FirstName = 'Robert' --1 value
+
+-- đáp án cho câu 3 bắt đầu
+-- select * from Employees where City = City quê của Robert
+select * from Employees where City =
+(select City from Employees where FirstName = 'Robert')
+
+-- câu này 9.9 điểm, trong kết quả còn Robert bị dư, tìm cùng quê Robert
+-- không cần nói lại Robert
+
+select * from Employees where City = 
+(select City from Employees where FirstName = 'Robert') and FirstName <> 'Robert'
+
+--4. liệt kê tất cả các đơn hàng
+select * from Orders order by Freight desc
+
+--4.1 liệt kê tất cả các đơn hàng trọng lượng lớn hơn 252kg
+select * from Orders where Freight >= 252
+
+--4.2 liệt kê tất cả các đơn  hàng có trọng lượng lớn hơn trọng lượng
+-- đơn hàng 10555
+select * from Orders where Freight >=
+(select Freight from Orders where OrderID = 10555) order by Freight desc --47 xuất hiện luôn cả 10555
+
+
+select * from Orders where Freight >=
+(select Freight from Orders where OrderID = 10555) and OrderID <> 10555 order by Freight desc 
+
+--BTVN Deadline: 23:00 22/9/2021 hoangnt2@fpt.edu.vn
+--1. Liệt kê danh sách các công ty vận chuyển hàng
+--2. Liệt kê danh sách các đơn hàng đc vận chuyển bởi công ty giao vận
+--có mã số 1
+--3. Liệt kê danh sách các đơn hàng đc vận chuyển bởi cty giao vận có tên Speedy Express I
+--4. Liệt kê danh sách các đơn hàng đc vận chuyển bởi cty giao vận
+--có tên Speedy Express và trọng lượng từ 50-100
+--5. Liệt kê các mặt hàng cùng chủng loại với mặt hàng Filo Mix
+--6. Liệt kê các nhân viên trẻ tuổi hơn nhân viên Janet
